94fe6957cd42d012ea2529ae9333f84d87cacb21
Ashutosh <ashu@gmail.com>
2021-02-07 12:16:46 +0530

    3 commit

--- pedia/pedia/settings.py
+++ pedia/pedia/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-
+import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -187,3 +187,7 @@ LOGIN_REDIRECT_URL = '/index/'
 # EMAIL_HOST_USER = 'singhashu2306@outlook.com'
 # EMAIL_HOST_PASSWORD = 'Amazing#1234'
 # EMAIL_PORT = 25
+
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
